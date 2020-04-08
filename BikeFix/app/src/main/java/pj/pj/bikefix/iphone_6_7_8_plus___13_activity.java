876be7138051b5d211cf43aa1700d3b549519a59
AnhHY <yenanh2822@gmail.com>
2020-04-08 09:07:46 +0700

    act13

--- BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___13_activity.java
+++ BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___13_activity.java
@@ -1,96 +1,84 @@
-
-	 
-	/*
-	 *	This content is generated from the API File Info.
-	 *	(Alt+Shift+Ctrl+I).
-	 *
-	 *	@desc 		
-	 *	@file 		iphone_6_7_8_plus___13
-	 *	@date 		0
-	 *	@title 		iPhone 6/7/8 Plus  13
-	 *	@author 	
-	 *	@keywords 	
-	 *	@generator 	Export Kit v1.2.9.xd
-	 *
-	 */
-	
-
 package pj.pj.bikefix;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
-
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class iphone_6_7_8_plus___13_activity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-	
-	private View _bg__iphone_6_7_8_plus___13;
-	private View rectangle_31;
-	private ImageView staticmap;
-	private View pin_bg_copy_2;
-	private View pin_bg_copy;
-	private View pin_bg;
-	private View rectangle_12;
-	private ImageView group_2;
-	private ImageView group_4;
-	private ImageView req;
-	private View rectangle_35;
-	private TextView damaged_wheel;
-	private ImageView icons8_high_importance_52px;
-	private View rectangle_36;
-	private TextView _56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh;
-	private ImageView icons8_place_marker_52px;
-	private View rectangle_44;
-	private View rectangle_45;
-	private ImageView l_p_3;
-	private TextView quality_evalution_;
-	private View rectangle_46;
-	private TextView your_comment_about_this_service___;
-	private View rectangle_47;
-	private TextView submit;
-	private ImageView icons8_add_camera_128px_1;
+public class iphone_6_7_8_plus___13_activity extends AppCompatActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.iphone_6_7_8_plus___13);
+    private View _bg__iphone_6_7_8_plus___13;
+    private View rectangle_31;
+    private ImageView staticmap;
+    private View pin_bg_copy_2;
+    private View pin_bg_copy;
+    private View pin_bg;
+    private View rectangle_12;
+    private ImageView group_2;
+    private ImageView group_4;
+    private ImageView req;
+    private View rectangle_35;
+    private TextView damaged_wheel;
+    private ImageView icons8_high_importance_52px;
+    private View rectangle_36;
+    private TextView _56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh;
+    private ImageView icons8_place_marker_52px;
+    private View rectangle_44;
+    private View rectangle_45;
+    private ImageView l_p_3;
+    private TextView quality_evalution_;
+    private View rectangle_46;
+    private TextView your_comment_about_this_service___;
+    private View rectangle_47;
+    private TextView submit;
+    private ImageView icons8_add_camera_128px_1;
 
-		
-		_bg__iphone_6_7_8_plus___13 = (View) findViewById(R.id._bg__iphone_6_7_8_plus___13);
-		rectangle_31 = (View) findViewById(R.id.rectangle_31);
-		staticmap = (ImageView) findViewById(R.id.staticmap);
-		pin_bg_copy_2 = (View) findViewById(R.id.pin_bg_copy_2);
-		pin_bg_copy = (View) findViewById(R.id.pin_bg_copy);
-		pin_bg = (View) findViewById(R.id.pin_bg);
-		rectangle_12 = (View) findViewById(R.id.rectangle_12);
-		group_2 = (ImageView) findViewById(R.id.group_2);
-		group_4 = (ImageView) findViewById(R.id.group_4);
-		req = (ImageView) findViewById(R.id.req);
-		rectangle_35 = (View) findViewById(R.id.rectangle_35);
-		damaged_wheel = (TextView) findViewById(R.id.damaged_wheel);
-		icons8_high_importance_52px = (ImageView) findViewById(R.id.icons8_high_importance_52px);
-		rectangle_36 = (View) findViewById(R.id.rectangle_36);
-		_56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh = (TextView) findViewById(R.id._56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh);
-		icons8_place_marker_52px = (ImageView) findViewById(R.id.icons8_place_marker_52px);
-		rectangle_44 = (View) findViewById(R.id.rectangle_44);
-		rectangle_45 = (View) findViewById(R.id.rectangle_45);
-		l_p_3 = (ImageView) findViewById(R.id.l_p_3);
-		quality_evalution_ = (TextView) findViewById(R.id.quality_evalution_);
-		rectangle_46 = (View) findViewById(R.id.rectangle_46);
-		your_comment_about_this_service___ = (TextView) findViewById(R.id.your_comment_about_this_service___);
-		rectangle_47 = (View) findViewById(R.id.rectangle_47);
-		submit = (TextView) findViewById(R.id.submit);
-		icons8_add_camera_128px_1 = (ImageView) findViewById(R.id.icons8_add_camera_128px_1);
-	
-		
-		//custom code goes here
-	
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.iphone_6_7_8_plus___13);
+
+
+        _bg__iphone_6_7_8_plus___13 = (View) findViewById(R.id._bg__iphone_6_7_8_plus___13);
+        rectangle_31 = (View) findViewById(R.id.rectangle_31);
+        staticmap = (ImageView) findViewById(R.id.staticmap);
+        pin_bg_copy_2 = (View) findViewById(R.id.pin_bg_copy_2);
+        pin_bg_copy = (View) findViewById(R.id.pin_bg_copy);
+        pin_bg = (View) findViewById(R.id.pin_bg);
+        rectangle_12 = (View) findViewById(R.id.rectangle_12);
+        group_2 = (ImageView) findViewById(R.id.group_2);
+        group_4 = (ImageView) findViewById(R.id.group_4);
+        req = (ImageView) findViewById(R.id.req);
+        rectangle_35 = (View) findViewById(R.id.rectangle_35);
+        damaged_wheel = (TextView) findViewById(R.id.damaged_wheel);
+        icons8_high_importance_52px = (ImageView) findViewById(R.id.icons8_high_importance_52px);
+        rectangle_36 = (View) findViewById(R.id.rectangle_36);
+        _56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh = (TextView) findViewById(R.id._56_l__v_n_vi_t__t_ng_nh_n_ph___qu_n_9__h__ch__minh);
+        icons8_place_marker_52px = (ImageView) findViewById(R.id.icons8_place_marker_52px);
+        rectangle_44 = (View) findViewById(R.id.rectangle_44);
+        rectangle_45 = (View) findViewById(R.id.rectangle_45);
+        l_p_3 = (ImageView) findViewById(R.id.l_p_3);
+        quality_evalution_ = (TextView) findViewById(R.id.quality_evalution_);
+        rectangle_46 = (View) findViewById(R.id.rectangle_46);
+        your_comment_about_this_service___ = (TextView) findViewById(R.id.your_comment_about_this_service___);
+        rectangle_47 = (View) findViewById(R.id.rectangle_47);
+        submit = (TextView) findViewById(R.id.submit);
+        icons8_add_camera_128px_1 = (ImageView) findViewById(R.id.icons8_add_camera_128px_1);
+
+
+        //custom code goes here
+
+    }
+
+    public void clickSubmit(View view) {
+        Intent intent = new Intent(this, iphone_6_7_8_plus___2_activity.class);
+        startActivity(intent);
+    }
 }
 	
 	
