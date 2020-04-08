2fd5c44ed3837fc2c75c532bd2b3abf2c7a6f1d6
Nguyễn Hiếu Đẹp Trai <46532530+nguyenhieudeptrai@users.noreply.github.com>
2020-04-08 11:07:31 +0700

    19

--- BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___19_activity.java
+++ BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___19_activity.java
@@ -1,122 +1,108 @@
-
-	 
-	/*
-	 *	This content is generated from the API File Info.
-	 *	(Alt+Shift+Ctrl+I).
-	 *
-	 *	@desc 		
-	 *	@file 		iphone_6_7_8_plus___19
-	 *	@date 		0
-	 *	@title 		iPhone 6/7/8 Plus  19
-	 *	@author 	
-	 *	@keywords 	
-	 *	@generator 	Export Kit v1.2.9.xd
-	 *
-	 */
-	
-
 package pj.pj.bikefix;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-
-
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class iphone_6_7_8_plus___19_activity extends AppCompatActivity {
 
-	
-	private View _bg__iphone_6_7_8_plus___19;
-	private ImageView path_4;
-	private View rectangle_16;
-	private TextView vietnam;
-	private ImageView polygon_3;
-	private TextView country__;
-	private View rectangle_16_ek1;
-	private TextView ho_chi_minh_city;
-	private ImageView polygon_3_ek1;
-	private TextView city__;
-	private ImageView line_11;
-	private TextView phone__;
-	private TextView enter_phone_number___;
-	private ImageView line_10;
-	private TextView full_name__;
-	private TextView enter_your_name___;
-	private ImageView line_11_ek1;
-	private TextView email_;
-	private TextView enter_email___;
-	private View rectangle_47;
-	private TextView sign_up;
-	private ImageView line_11_ek2;
-	private TextView address_;
-	private TextView enter_address___;
-	private View rectangle_51;
-	private TextView i_accept_the_terms_in_license_agreement_with_the_policy;
-	private TextView register;
-	private View rectangle_47_ek1;
-	private TextView login;
-	private TextView bikefix;
-	private ImageView _2_copy;
-	private ImageView line_10_ek1;
-	private TextView password__;
-	private TextView enter_password___;
-	private ImageView line_10_ek2;
-	private TextView re_password__;
-	private TextView enter_re_password___;
-	private ImageView line_16;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
+    private View _bg__iphone_6_7_8_plus___19;
+    private ImageView path_4;
+    private View rectangle_16;
+    private TextView vietnam;
+    private ImageView polygon_3;
+    private TextView country__;
+    private View rectangle_16_ek1;
+    private TextView ho_chi_minh_city;
+    private ImageView polygon_3_ek1;
+    private TextView city__;
+    private ImageView line_11;
+    private TextView phone__;
+    private TextView enter_phone_number___;
+    private ImageView line_10;
+    private TextView full_name__;
+    private TextView enter_your_name___;
+    private ImageView line_11_ek1;
+    private TextView email_;
+    private TextView enter_email___;
+    private View rectangle_47;
+    private TextView sign_up;
+    private ImageView line_11_ek2;
+    private TextView address_;
+    private TextView enter_address___;
+    private View rectangle_51;
+    private TextView i_accept_the_terms_in_license_agreement_with_the_policy;
+    private TextView register;
+    private View rectangle_47_ek1;
+    private TextView login;
+    private TextView bikefix;
+    private ImageView _2_copy;
+    private ImageView line_10_ek1;
+    private TextView password__;
+    private TextView enter_password___;
+    private ImageView line_10_ek2;
+    private TextView re_password__;
+    private TextView enter_re_password___;
+    private ImageView line_16;
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.iphone_6_7_8_plus___19);
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
 
-		
-		_bg__iphone_6_7_8_plus___19 = (View) findViewById(R.id._bg__iphone_6_7_8_plus___19);
-		path_4 = (ImageView) findViewById(R.id.path_4);
-		rectangle_16 = (View) findViewById(R.id.rectangle_16);
-		vietnam = (TextView) findViewById(R.id.vietnam);
-		polygon_3 = (ImageView) findViewById(R.id.polygon_3);
-		country__ = (TextView) findViewById(R.id.country__);
-		rectangle_16_ek1 = (View) findViewById(R.id.rectangle_16_ek1);
-		ho_chi_minh_city = (TextView) findViewById(R.id.ho_chi_minh_city);
-		polygon_3_ek1 = (ImageView) findViewById(R.id.polygon_3_ek1);
-		city__ = (TextView) findViewById(R.id.city__);
-		line_11 = (ImageView) findViewById(R.id.line_11);
-		phone__ = (TextView) findViewById(R.id.phone__);
-		enter_phone_number___ = (TextView) findViewById(R.id.enter_phone_number___);
-		line_10 = (ImageView) findViewById(R.id.line_10);
-		full_name__ = (TextView) findViewById(R.id.full_name__);
-		enter_your_name___ = (TextView) findViewById(R.id.enter_your_name___);
-		line_11_ek1 = (ImageView) findViewById(R.id.line_11_ek1);
-		email_ = (TextView) findViewById(R.id.email_);
-		enter_email___ = (TextView) findViewById(R.id.enter_email___);
-		rectangle_47 = (View) findViewById(R.id.rectangle_47);
-		sign_up = (TextView) findViewById(R.id.sign_up);
-		line_11_ek2 = (ImageView) findViewById(R.id.line_11_ek2);
-		address_ = (TextView) findViewById(R.id.address_);
-		enter_address___ = (TextView) findViewById(R.id.enter_address___);
-		rectangle_51 = (View) findViewById(R.id.rectangle_51);
-		i_accept_the_terms_in_license_agreement_with_the_policy = (TextView) findViewById(R.id.i_accept_the_terms_in_license_agreement_with_the_policy);
-		register = (TextView) findViewById(R.id.register);
-		rectangle_47_ek1 = (View) findViewById(R.id.rectangle_47_ek1);
-		login = (TextView) findViewById(R.id.login);
-		bikefix = (TextView) findViewById(R.id.bikefix);
-		_2_copy = (ImageView) findViewById(R.id._2_copy);
-		line_10_ek1 = (ImageView) findViewById(R.id.line_10_ek1);
-		password__ = (TextView) findViewById(R.id.password__);
-		enter_password___ = (TextView) findViewById(R.id.enter_password___);
-		line_10_ek2 = (ImageView) findViewById(R.id.line_10_ek2);
-		re_password__ = (TextView) findViewById(R.id.re_password__);
-		enter_re_password___ = (TextView) findViewById(R.id.enter_re_password___);
-		line_16 = (ImageView) findViewById(R.id.line_16);
-	
-		
-		//custom code goes here
-	
-	}
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.iphone_6_7_8_plus___19);
+
+
+        _bg__iphone_6_7_8_plus___19 = (View) findViewById(R.id._bg__iphone_6_7_8_plus___19);
+        path_4 = (ImageView) findViewById(R.id.path_4);
+        rectangle_16 = (View) findViewById(R.id.rectangle_16);
+        vietnam = (TextView) findViewById(R.id.vietnam);
+        polygon_3 = (ImageView) findViewById(R.id.polygon_3);
+        country__ = (TextView) findViewById(R.id.country__);
+        rectangle_16_ek1 = (View) findViewById(R.id.rectangle_16_ek1);
+        ho_chi_minh_city = (TextView) findViewById(R.id.ho_chi_minh_city);
+        polygon_3_ek1 = (ImageView) findViewById(R.id.polygon_3_ek1);
+        city__ = (TextView) findViewById(R.id.city__);
+        line_11 = (ImageView) findViewById(R.id.line_11);
+        phone__ = (TextView) findViewById(R.id.phone__);
+        enter_phone_number___ = (TextView) findViewById(R.id.enter_phone_number___);
+        line_10 = (ImageView) findViewById(R.id.line_10);
+        full_name__ = (TextView) findViewById(R.id.full_name__);
+        enter_your_name___ = (TextView) findViewById(R.id.enter_your_name___);
+        line_11_ek1 = (ImageView) findViewById(R.id.line_11_ek1);
+        email_ = (TextView) findViewById(R.id.email_);
+        enter_email___ = (TextView) findViewById(R.id.enter_email___);
+        rectangle_47 = (View) findViewById(R.id.rectangle_47);
+        sign_up = (TextView) findViewById(R.id.sign_up);
+        line_11_ek2 = (ImageView) findViewById(R.id.line_11_ek2);
+        address_ = (TextView) findViewById(R.id.address_);
+        enter_address___ = (TextView) findViewById(R.id.enter_address___);
+        rectangle_51 = (View) findViewById(R.id.rectangle_51);
+        i_accept_the_terms_in_license_agreement_with_the_policy = (TextView) findViewById(R.id.i_accept_the_terms_in_license_agreement_with_the_policy);
+        register = (TextView) findViewById(R.id.register);
+        rectangle_47_ek1 = (View) findViewById(R.id.rectangle_47_ek1);
+        login = (TextView) findViewById(R.id.login);
+        bikefix = (TextView) findViewById(R.id.bikefix);
+        _2_copy = (ImageView) findViewById(R.id._2_copy);
+        line_10_ek1 = (ImageView) findViewById(R.id.line_10_ek1);
+        password__ = (TextView) findViewById(R.id.password__);
+        enter_password___ = (TextView) findViewById(R.id.enter_password___);
+        line_10_ek2 = (ImageView) findViewById(R.id.line_10_ek2);
+        re_password__ = (TextView) findViewById(R.id.re_password__);
+        enter_re_password___ = (TextView) findViewById(R.id.enter_re_password___);
+        line_16 = (ImageView) findViewById(R.id.line_16);
+
+
+        //custom code goes here
+
+    }
+
+    public void clickLogin(View view) {
+        finish();
+    }
 }
 	
 	
