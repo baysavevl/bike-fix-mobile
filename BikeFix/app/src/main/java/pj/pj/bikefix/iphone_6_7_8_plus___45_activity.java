33cc62da77f581128251a3edc5907af71fc6d134
Nguyễn Hiếu Đẹp Trai <46532530+nguyenhieudeptrai@users.noreply.github.com>
2020-04-10 01:58:51 +0700

    45.47

--- BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___45_activity.java
+++ BikeFix/app/src/main/java/pj/pj/bikefix/iphone_6_7_8_plus___45_activity.java
@@ -1,86 +1,70 @@
-
-	 
-	/*
-	 *	This content is generated from the API File Info.
-	 *	(Alt+Shift+Ctrl+I).
-	 *
-	 *	@desc 		
-	 *	@file 		iphone_6_7_8_plus___45
-	 *	@date 		0
-	 *	@title 		iPhone 6/7/8 Plus  45
-	 *	@author 	
-	 *	@keywords 	
-	 *	@generator 	Export Kit v1.2.9.xd
-	 *
-	 */
-	
-
 package pj.pj.bikefix;
 
-import android.app.Activity;
 import android.os.Bundle;
-
-
 import android.view.View;
-import android.widget.TextView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class iphone_6_7_8_plus___45_activity extends Activity {
+import androidx.appcompat.app.AppCompatActivity;
 
-	
-	private View _bg__iphone_6_7_8_plus___45;
-	private View rectangle_copy_2;
-	private TextView emily_daniels;
-	private TextView pozna___poland;
-	private ImageView istockphoto_1003393752_170667a;
-	private TextView phone__;
-	private TextView __84_963_192_405;
-	private ImageView line_10;
-	private TextView email_;
-	private TextView my_google_gmail_com;
-	private ImageView line_10_ek1;
-	private TextView address_;
-	private TextView s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_;
-	private ImageView line_10_ek2;
-	private View rectangle_37;
-	private ImageView line_6;
-	private ImageView path_1;
-	private TextView account;
-	private View rectangle_38;
-	private TextView save;
+public class iphone_6_7_8_plus___45_activity extends AppCompatActivity {
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.iphone_6_7_8_plus___45);
+    private View _bg__iphone_6_7_8_plus___45;
+    private View rectangle_copy_2;
+    private TextView emily_daniels;
+    private TextView pozna___poland;
+    private ImageView istockphoto_1003393752_170667a;
+    private TextView phone__;
+    private TextView __84_963_192_405;
+    private ImageView line_10;
+    private TextView email_;
+    private TextView my_google_gmail_com;
+    private ImageView line_10_ek1;
+    private TextView address_;
+    private TextView s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_;
+    private ImageView line_10_ek2;
+    private View rectangle_37;
+    private ImageView line_6;
+    private ImageView path_1;
+    private TextView account;
+    private View rectangle_38;
+    private TextView save;
 
-		
-		_bg__iphone_6_7_8_plus___45 = (View) findViewById(R.id._bg__iphone_6_7_8_plus___45);
-		rectangle_copy_2 = (View) findViewById(R.id.rectangle_copy_2);
-		emily_daniels = (TextView) findViewById(R.id.emily_daniels);
-		pozna___poland = (TextView) findViewById(R.id.pozna___poland);
-		istockphoto_1003393752_170667a = (ImageView) findViewById(R.id.istockphoto_1003393752_170667a);
-		phone__ = (TextView) findViewById(R.id.phone__);
-		__84_963_192_405 = (TextView) findViewById(R.id.__84_963_192_405);
-		line_10 = (ImageView) findViewById(R.id.line_10);
-		email_ = (TextView) findViewById(R.id.email_);
-		my_google_gmail_com = (TextView) findViewById(R.id.my_google_gmail_com);
-		line_10_ek1 = (ImageView) findViewById(R.id.line_10_ek1);
-		address_ = (TextView) findViewById(R.id.address_);
-		s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_ = (TextView) findViewById(R.id.s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_);
-		line_10_ek2 = (ImageView) findViewById(R.id.line_10_ek2);
-		rectangle_37 = (View) findViewById(R.id.rectangle_37);
-		line_6 = (ImageView) findViewById(R.id.line_6);
-		path_1 = (ImageView) findViewById(R.id.path_1);
-		account = (TextView) findViewById(R.id.account);
-		rectangle_38 = (View) findViewById(R.id.rectangle_38);
-		save = (TextView) findViewById(R.id.save);
-	
-		
-		//custom code goes here
-	
-	}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.iphone_6_7_8_plus___45);
+
+
+        rectangle_copy_2 = (View) findViewById(R.id.rectangle_copy_2);
+        emily_daniels = (TextView) findViewById(R.id.emily_daniels);
+        pozna___poland = (TextView) findViewById(R.id.pozna___poland);
+        istockphoto_1003393752_170667a = (ImageView) findViewById(R.id.istockphoto_1003393752_170667a);
+        phone__ = (TextView) findViewById(R.id.phone__);
+        __84_963_192_405 = (TextView) findViewById(R.id.__84_963_192_405);
+        line_10 = (ImageView) findViewById(R.id.line_10);
+        email_ = (TextView) findViewById(R.id.email_);
+        my_google_gmail_com = (TextView) findViewById(R.id.my_google_gmail_com);
+        line_10_ek1 = (ImageView) findViewById(R.id.line_10_ek1);
+        address_ = (TextView) findViewById(R.id.address_);
+        s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_ = (TextView) findViewById(R.id.s__12_____ng_d1__ph__ng_8___g__v_p__tp_h__ch__minh_);
+        line_10_ek2 = (ImageView) findViewById(R.id.line_10_ek2);
+        rectangle_37 = (View) findViewById(R.id.rectangle_37);
+        line_6 = (ImageView) findViewById(R.id.line_6);
+        path_1 = (ImageView) findViewById(R.id.path_1);
+        account = (TextView) findViewById(R.id.account);
+        save = (TextView) findViewById(R.id.save);
+
+
+        //custom code goes here
+
+    }
+
+    public void clickSubmit(View view) {
+        finish();
+    }
 }
 	
 	
